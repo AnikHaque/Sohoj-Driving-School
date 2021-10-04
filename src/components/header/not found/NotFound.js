1194b828563758ec5497dc089834d7ca8115130e
EkramulHaqueEngineer <ekramulhaqueengineer@gmail.com>
2021-10-04 23:18:52 +0600

    added

--- src/components/header/not found/NotFound.js
+++ src/components/header/not found/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div>
-            <h1>404 NOT FOUND</h1>
+            <h1 className="text-center fw-bold">404 NOT FOUND</h1>
         </div>
     );
 };
