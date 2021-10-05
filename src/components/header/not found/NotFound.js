bbf5aa8f34b1822237a54cd55705a6a37b871e69
EkramulHaqueEngineer <ekramulhaqueengineer@gmail.com>
2021-10-05 22:36:55 +0600

    added

--- src/components/header/not found/NotFound.js
+++ src/components/header/not found/NotFound.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import './NotFound.css'
 const NotFound = () => {
     return (
         <div>
-            <h1 className="text-center fw-bold">404 NOT FOUND</h1>
+            <img src="https://png.pngtree.com/png-vector/20201224/ourlarge/pngtree-error-404-page-not-found-png-image_2598541.jpg" className="img-fluid notfound"></img>
+<br></br>
+            <button type="button" className="btn btn-primary notfound-button"><a href="https://sohoj-driving.netlify.app" className="text-white">back to home</a></button>
+
+
         </div>
     );
 };
